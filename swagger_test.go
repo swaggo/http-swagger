6be81482e6f05ee7febc589c076e7c17b2eb902b
pei0804 <peeeei0804@gmail.com>
2018-02-26 13:12:07 +0900

    add http-swagger

---
+++ swagger_test.go
@@ -0,0 +1,37 @@
+package httpSwagger
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi"
+	"github.com/stretchr/testify/assert"
+	_ "github.com/swaggo/http-swagger/example/docs"
+)
+
+func TestWrapHandler(t *testing.T) {
+	router := chi.NewRouter()
+
+	router.Get("/*", WrapHandler)
+
+	w1 := performRequest("GET", "/index.html", router)
+	assert.Equal(t, 200, w1.Code)
+
+	w2 := performRequest("GET", "/doc.json", router)
+	assert.Equal(t, 200, w2.Code)
+
+	w3 := performRequest("GET", "/favicon-16x16.png", router)
+	assert.Equal(t, 200, w3.Code)
+
+	w4 := performRequest("GET", "/notfound", router)
+	assert.Equal(t, 404, w4.Code)
+}
+
+func performRequest(method, target string, h http.Handler) *httptest.ResponseRecorder {
+	r := httptest.NewRequest(method, target, nil)
+	w := httptest.NewRecorder()
+
+	h.ServeHTTP(w, r)
+	return w
+}
