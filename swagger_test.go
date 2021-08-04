b095f38cabcdf6b4d6cd830bf5c006ef039e3ebb
Bogdan Ungureanu <ubogdan@gmail.com>
2021-08-04 22:05:13 +0300

    * improve unit tests

--- swagger_test.go
+++ swagger_test.go
@@ -7,18 +7,48 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/stretchr/testify/assert"
-	_ "github.com/swaggo/http-swagger/example/go-chi/docs"
+	"github.com/swaggo/swag"
 )
+
+type mockedSwag struct{}
+
+func (s *mockedSwag) ReadDoc() string {
+	return `{
+    "swagger": "2.0",
+    "info": {
+        "description": "This is a sample server Petstore server.",
+        "title": "Swagger Example API",
+        "termsOfService": "http://swagger.io/terms/",
+        "contact": {
+            "name": "API Support",
+            "url": "http://www.swagger.io/support",
+            "email": "support@swagger.io"
+        },
+        "license": {
+            "name": "Apache 2.0",
+            "url": "http://www.apache.org/licenses/LICENSE-2.0.html"
+        },
+        "version": "1.0"
+    },
+    "host": "petstore.swagger.io",
+    "basePath": "/v2",
+    "paths": {}
+}`
+}
 
 func TestWrapHandler(t *testing.T) {
 	router := chi.NewRouter()
 
-	router.Get("/*", WrapHandler)
+	router.Get("/*", Handler(DocExpansion("none"), DomID("#swagger-ui")))
 
 	w1 := performRequest("GET", "/index.html", router)
 	assert.Equal(t, 200, w1.Code)
 
-	w2 := performRequest("GET", "/doc.json", router)
+	w2 := performRequest("GET", "/mockedSwag.json", router)
+	assert.Equal(t, 500, w2.Code)
+
+	swag.Register(swag.Name, &mockedSwag{})
+	w2 = performRequest("GET", "/mockedSwag.json", router)
 	assert.Equal(t, 200, w2.Code)
 	assert.Equal(t, "application/json; charset=utf-8", w2.Header().Get("content-type"))
 
@@ -30,18 +60,6 @@ func TestWrapHandler(t *testing.T) {
 
 	w5 := performRequest("GET", "/", router)
 	assert.Equal(t, 301, w5.Code)
-}
-
-func TestHandler(t *testing.T) {
-	router := chi.NewRouter()
-	router.Get("/*", Handler(DocExpansion("none"), DomID("#swagger-ui")))
-
-	w1 := performRequest("GET", "/index.html", router)
-	assert.Equal(t, 200, w1.Code)
-	w2 := performRequest("GET", "/doc.json", router)
-
-	assert.Equal(t, 200, w2.Code)
-	assert.Equal(t, "application/json; charset=utf-8", w2.Header().Get("content-type"))
 }
 
 func performRequest(method, target string, h http.Handler) *httptest.ResponseRecorder {
